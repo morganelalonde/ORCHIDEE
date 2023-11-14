24b5066a787629646f706af1629d9911ed56c7a5
Morgane Lalonde <morgane.lalonde-le-pajolec@latmos.ipsl.fr>
2023-11-14 15:59:56 +0100

    SAVE frac_imperv

--- src_sechiba/slowproc.f90
+++ src_sechiba/slowproc.f90
@@ -85,7 +85,7 @@ MODULE slowproc
 !$OMP THREADPRIVATE(lcanop)
   INTEGER(i_std) , SAVE                              :: veget_year          !! year for vegetation update
 !$OMP THREADPRIVATE(veget_year)
-  REAL(r_std), ALLOCATABLE, DIMENSION(:)             :: frac_imperv       !! Imperviousness fraction of each gridcell
+  REAL(r_std), ALLOCATABLE, SAVE, DIMENSION(:)       :: frac_imperv       !! Imperviousness fraction of each gridcell
 
 CONTAINS
 
